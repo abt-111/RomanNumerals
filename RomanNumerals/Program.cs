c2d53e967733ef72335b962620515188cf714da3
abt93 <anthony.temmel@gmail.com>
2024-06-10 16:00:54 +0200

    Ajout d'un premier filtre des entrées de l'utilisateur

--- RomanNumerals/Program.cs
+++ RomanNumerals/Program.cs
@@ -1,21 +1,37 @@
-﻿class MainClass
+﻿using System;
+using System.Text.RegularExpressions;
+
+class MainClass
 {
     public static void Main(string[] args)
     {
         int convertedTotal = 0;
         string romanNumerals; // Chiffres entrée par l'utilisateur
+        string pattern = "^[MDCLXVI]*$";
         List<string> decomposedRomanNumerals = new List<string>();
 
-        // Saisie de l'utilisateur (pour l'instant on suppose que l'entrée est toujours correct)
+        // Saisie de l'utilisateur
         Console.Write("Veuillez entrez un chiffre romains : ");
+        
         romanNumerals = Console.ReadLine();
 
+
+        // On vérifie que l'utilisateur ne rentre que des chiffres romains (on ne vérifie pas l'order)
+        if (Regex.IsMatch(romanNumerals, pattern))
+        {
+            Console.WriteLine("Valide");
+        }
+        else
+        {
+            Console.WriteLine("Invalide");
+        }
+
         // Décomposition des chiffres romains
-        Fonctions.RomanNumeralsDecomposition(romanNumerals, decomposedRomanNumerals);
+        //Fonctions.RomanNumeralsDecomposition(romanNumerals, decomposedRomanNumerals);
 
         // Conversion des chiffres romains et renvoie de la somme
-        convertedTotal = Fonctions.RomansToDecimal(decomposedRomanNumerals);
+        //convertedTotal = Fonctions.RomansToDecimal(decomposedRomanNumerals);
 
-        Console.WriteLine("Résultat de la conversion : " + convertedTotal);
+        //Console.WriteLine("Résultat de la conversion : " + convertedTotal);
     }
 }
