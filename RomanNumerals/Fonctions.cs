70549bd1156c157f7f8a4178c7325b4d5a10aad2
abt93 <anthony.temmel@gmail.com>
2024-06-10 15:28:35 +0200

    Séparation du code dans une autre classe appellée Fonctions

---
+++ RomanNumerals/Fonctions.cs
@@ -0,0 +1,110 @@
+﻿public static class Fonctions
+{
+    public static void RomanNumeralsDecomposition(string romanNumerals, List<string> decomposedRomanNumerals)
+    {
+        string decomposedRomanNumeral;
+
+        for (int i = 0; i < romanNumerals.Length; i++)
+        {
+            decomposedRomanNumeral = "";
+            decomposedRomanNumeral += romanNumerals[i];
+
+            // Traitement des cas particuliers de CM, CD, XC, XL, IX et IV
+            if ((i + 1) < romanNumerals.Length)
+            {
+                if (romanNumerals[i] != romanNumerals[i + 1])
+                {
+                    switch (decomposedRomanNumeral)
+                    {
+                        case "C":
+                            if (romanNumerals[i + 1] == 'M' || romanNumerals[i + 1] == 'D')
+                            {
+                                decomposedRomanNumeral += romanNumerals[i + 1];
+                                i++;
+                            }
+                            break;
+                        case "X":
+                            if (romanNumerals[i + 1] == 'C' || romanNumerals[i + 1] == 'L')
+                            {
+                                decomposedRomanNumeral += romanNumerals[i + 1];
+                                i++;
+                            }
+                            break;
+                        case "I":
+                            if (romanNumerals[i + 1] == 'X' || romanNumerals[i + 1] == 'V')
+                            {
+                                decomposedRomanNumeral += romanNumerals[i + 1];
+                                i++;
+                            }
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+            decomposedRomanNumerals.Add(decomposedRomanNumeral);
+        }
+    }
+
+    public static int RomansToDecimal(List<string> decomposedRomanNumerals)
+    {
+        int convertedTotal = 0;
+
+        foreach (string romanNumeralDecomposed in decomposedRomanNumerals)
+        {
+            Console.WriteLine(romanNumeralDecomposed);
+            convertedTotal += Fonctions.RomanToDecimal(romanNumeralDecomposed);
+        }
+
+        return convertedTotal;
+    }
+
+    public static int RomanToDecimal(string args)
+    {
+        switch (args)
+        {
+            case "I":
+                return 1;
+                break;
+            case "IV":
+                return 4;
+                break;
+            case "V":
+                return 5;
+                break;
+            case "IX":
+                return 9;
+                break;
+            case "X":
+                return 10;
+                break;
+            case "XL":
+                return 40;
+                break;
+            case "L":
+                return 50;
+                break;
+            case "XC":
+                return 90;
+                break;
+            case "C":
+                return 100;
+                break;
+            case "CD":
+                return 400;
+                break;
+            case "D":
+                return 500;
+                break;
+            case "CM":
+                return 900;
+                break;
+            case "M":
+                return 1000;
+                break;
+            default:
+                return 0;
+                break;
+        }
+    }
+}
--- RomanNumerals/Program.cs
+++ RomanNumerals/Program.cs
@@ -2,107 +2,20 @@
 {
     public static void Main(string[] args)
     {
-        int resultatConversion = 0;
-        string romanNumeral;
-        string decomposition;
-        List<string> romanNumeralsDecomposed = new List<string>();
+        int convertedTotal = 0;
+        string romanNumerals; // Chiffres entrée par l'utilisateur
+        List<string> decomposedRomanNumerals = new List<string>();
 
         // Saisie de l'utilisateur (pour l'instant on suppose que l'entrée est toujours correct)
         Console.Write("Veuillez entrez un chiffre romains : ");
-        romanNumeral = Console.ReadLine();
+        romanNumerals = Console.ReadLine();
 
-        for(int i = 0; i < romanNumeral.Length; i++)
-        {
-            decomposition = "";
-            decomposition += romanNumeral[i];
+        // Décomposition des chiffres romains
+        Fonctions.RomanNumeralsDecomposition(romanNumerals, decomposedRomanNumerals);
 
-            if ((i + 1) < romanNumeral.Length)
-            if (romanNumeral[i] != romanNumeral[i + 1])
-            {
-                switch (decomposition)
-                {
-                    case "C":
-                        if (romanNumeral[i + 1] == 'M' || romanNumeral[i + 1] == 'D')
-                        {
-                            decomposition += romanNumeral[i + 1];
-                            i++;
-                        }
-                        break;
-                    case "X":
-                        if (romanNumeral[i + 1] == 'C' || romanNumeral[i + 1] == 'L')
-                        {
-                            decomposition += romanNumeral[i + 1];
-                            i++;
-                        }
-                        break;
-                    case "I":
-                        if (romanNumeral[i + 1] == 'X' || romanNumeral[i + 1] == 'V')
-                        {
-                            decomposition += romanNumeral[i + 1];
-                            i++;
-                        }
-                        break;
-                    default:
-                        break;
-                }
-            }
-            romanNumeralsDecomposed.Add(decomposition);
-        }
-        foreach (string romanNumeralDecomposed in romanNumeralsDecomposed)
-        {
-            Console.WriteLine(romanNumeralDecomposed);
-            resultatConversion += ConvertValue(romanNumeralDecomposed);
-        }
+        // Conversion des chiffres romains et renvoie de la somme
+        convertedTotal = Fonctions.RomansToDecimal(decomposedRomanNumerals);
 
-        Console.WriteLine("Résultat de la conversion : " + resultatConversion);
-    }
-
-    public static int ConvertValue(string args)
-    {
-        switch(args)
-        {
-            case "I":
-                return 1;
-                break;
-            case "IV":
-                return 4;
-                break;
-            case "V":
-                return 5;
-                break;
-            case "IX":
-                return 9;
-                break;
-            case "X":
-                return 10;
-                break;
-            case "XL":
-                return 40;
-                break;
-            case "L":
-                return 50;
-                break;
-            case "XC":
-                return 90;
-                break;
-            case "C":
-                return 100;
-                break;
-            case "CD":
-                return 400;
-                break;
-            case "D":
-                return 500;
-                break;
-            case "CM":
-                return 900;
-                break;
-            case "M":
-                return 1000;
-                break;
-            default:
-                return 0;
-                break;
-        }
+        Console.WriteLine("Résultat de la conversion : " + convertedTotal);
     }
 }
