11c70377edcfb11b313409e16c908ef4226f4734
abt93 <anthony.temmel@gmail.com>
2024-06-10 17:23:09 +0200

    Ajout validation de l'expression régulière pour valider le chiffre romain

--- RomanNumerals/Program.cs
+++ RomanNumerals/Program.cs
@@ -7,23 +7,36 @@ class MainClass
     {
         int convertedTotal = 0;
         string romanNumerals; // Chiffres entrée par l'utilisateur
-        string pattern = "^[MDCLXVI]*$";
+        string pattern1 = "^[MDCLXVI]*$";
+        string pattern2 = "^[M]{0,3}([D]?[C]{0,3}|CM|CD)([L]?[X]{0,3}|XC|XL)([V]?[I]{0,3}|IX|IV)$";
         List<string> decomposedRomanNumerals = new List<string>();
 
         // Saisie de l'utilisateur
         Console.Write("Veuillez entrez un chiffre romains : ");
-        
-        romanNumerals = Console.ReadLine();
 
+        while(true)
+        {
+            romanNumerals = Console.ReadLine();
 
-        // On vérifie que l'utilisateur ne rentre que des chiffres romains (on ne vérifie pas l'order)
-        if (Regex.IsMatch(romanNumerals, pattern))
-        {
-            Console.WriteLine("Valide");
-        }
-        else
-        {
-            Console.WriteLine("Invalide");
+            // On vérifie que l'utilisateur ne rentre que des chiffres romains (pas la validité de l'ordre etc)
+            if (Regex.IsMatch(romanNumerals, pattern1))
+            {
+                Console.WriteLine("Valide");
+            }
+            else
+            {
+                Console.WriteLine("Invalide");
+            }
+
+            // On vérifie la validité de l'ordre etc
+            if (Regex.IsMatch(romanNumerals, pattern2))
+            {
+                Console.WriteLine("Valide");
+            }
+            else
+            {
+                Console.WriteLine("Invalide");
+            }
         }
 
         // Décomposition des chiffres romains
